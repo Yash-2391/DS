c024c0b491d216cbe4985ae6c74d42e62f5ff231
yash gohil <yashgohil2391@gmail.com>
2022-01-23 19:45:12 -0500

    SEARCH AND SWAPNODE USING CLLECTION FRAMEWORK

--- DSLinkedListp1.java
+++ DSLinkedListp1.java
@@ -13,9 +13,38 @@ class DSLinkedListp1{
         } 
         System.out.println("ENTER THE SEARCH VALUE");
         int key=s.nextInt();
-        for(int i=0;i<n;i++){
-            if(l.get(i)==key)
-                System.out.println("ELEMENT IS PRESENT AT POSITION "+ (i+1));
+        searchElement(l,key);
+        System.out.println("THE LIST IS");
+        for(int i : l){
+            System.out.print(i+"->");
         }
+        System.out.print("null");
+        System.out.println();
+        System.out.println("ENTER THE TWO ELEMENTS YOU WANT SWAP");
+        int x,y;
+        x=s.nextInt();
+        y=s.nextInt();
+        SwapList(l,x,y);
+
+    }
+    public static void searchElement(java.util.LinkedList<Integer> l,int key){
+        for(int i=0;i<l.size();i++){
+            if(l.get(i)==key){
+                System.out.println("ELEMENT FOUND AT "+(i+1)+"th INDEX");
+                break;
+            }
+        }
+    } 
+    public static void SwapList(java.util.LinkedList<Integer> l,int x,int y){
+        int indx=l.indexOf(x);
+        int indy=l.indexOf(y);
+        l.set(indx,y);
+        l.set(indy,x);
+        System.out.println("THE SWAPPED LIST IS");
+        for(int i : l){
+            System.out.print(i+"->");
+        }
+        System.out.print("null");
+        System.out.println();
     }
 }
